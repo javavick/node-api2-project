d3bdba1cc5f303e30460d64085161afe537b6d67
Vick Javariz <51038821+javavick@users.noreply.github.com>
2020-01-21 21:39:26 -0400

    GET for '/api/posts/:id'

--- api/apiRouter.js
+++ api/apiRouter.js
@@ -72,6 +72,25 @@ router.get("/", (req, res) => {
     );
 });
 
+// GET ("/api/posts/:id")
+router.get("/:id", (req, res) => {
+  Data.findById(req.params.id)
+    .then((post) => {
+      if (post.length > 0) {
+        res.status(200).json(post);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch(() =>
+      res
+        .status(500)
+        .json({ error: "The post information could not be retrieved." })
+    );
+});
+
 // GET ("/api/posts/:id/comments")
 router.get("/:id/comments", (req, res) => {
   Data.findById(req.params.id)
